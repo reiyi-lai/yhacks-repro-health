a9ef36ed0be677b30d6c09fbe31b0be42dbb0471
christine <107821719+chrixti@users.noreply.github.com>
2024-03-30 15:32:59 -0400

    sort.py completed

--- backend/sort.py
+++ backend/sort.py
@@ -16,16 +16,6 @@ with open('./data/symptoms.csv', mode='r') as csvfile:
                     if symptom not in symptomsList:  # dupe check
                         symptomsList.append(symptom)
                         dsMap[row['Disease Name']] = symptoms
-                        
-def identify_potential_diseases(initialSymptom):
-    potentialDiseases = []
-    for disease, symptoms in dsMap.items():
-        if initialSymptom in symptoms:
-            potentialDiseases.append(disease)
-            print("potentialDiseases", potentialDiseases)
-
-    return potentialDiseases
-
 
 
 #print("Diseases:", diseasesList)

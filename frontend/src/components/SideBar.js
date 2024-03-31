4c97aab18eba1d316e5df883518dcfdc4d4418f7
reiyi-lai <83943128+reiyi-lai@users.noreply.github.com>
2024-03-30 20:55:54 -0400

    First Commit for New Frontend

---
+++ frontend/src/components/SideBar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Sidebar.css'; // Import your CSS file for styling
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <h2>Chat App</h2>
+      <ul>
+        <li>New Chat</li>
+        <li>Chat 1</li>
+        <li>Chat 2</li>
+        <li>Chat 3</li>
+        {/* Add more list items for additional chats */}
+      </ul>
+    </div>
+  );
+}
+
+export default Sidebar;

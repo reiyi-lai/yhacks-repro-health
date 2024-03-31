d0a11d43ec7dab15752e073f71d4b8b0f4fc15d3
reiyi-lai <83943128+reiyi-lai@users.noreply.github.com>
2024-03-30 21:50:13 -0400

    Commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,21 @@
 import './App.css';
 import React from 'react';
 import Sidebar from './components/SideBar.js';
-import ChatMessage from './components/ChatMessage.js';
+import Chatbubbles from './components/ChatBubbles.js';
+import Chatinputbar from './components/ChatInputBar.js';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app-container">
       <Sidebar />
+      <div className="chat-container">
+        <div className="chat-view">
+          <Chatinputbar text="Hello! How can I assist you?" isBot={true} />
+          <Chatinputbar text="Hi! I have a question about..." isBot={false} />
+          {/* Add more chat bubbles */}
+        </div>
+        <ChatInputBar onSubmit={handleSendMessage} /> {/* Add the ChatInputBar component */}
+      </div>
     </div>
   );
 }

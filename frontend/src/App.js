90d24be172d54ed894f803eed4c71e7da479d1b9
reiyi-lai <83943128+reiyi-lai@users.noreply.github.com>
2024-03-31 12:09:07 -0400

    Commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,38 +1,16 @@
 import axios from "axios";
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import Sidebar from './components/SideBar.js';
 import ChatBubble from './components/ChatBubbles.js';
 import ChatInputBar from './components/ChatInputBar.js';
 import { fetchAPI } from './services/api';
 
 function ChatApp() {
-  const [data, setData] = useState('');
-  const [loading, setLoading] = useState(false);
-
-
-  useEffect(() => {
-    const fetchData = async () => {
-      if(loading){
-        fetchAPI("poem").
-          then(res => {
-          setData(res);
-          setLoading(false);
-        });
-      }
-
-    };
-    fetchData();
-  }, [loading]);
-
   const [messages, setMessages] = useState([]);
 
   const handleNewMessage = (message) => {
     // Handle sending message logic here (e.g., sending message to chat server)
     setMessages([...messages, { text: message, isBot: false }]);
-  };
-
-  const handleLoading = () => {
-    setLoading(true);
   };
 
   return (
@@ -47,8 +25,6 @@ function ChatApp() {
         </div>
         <ChatInputBar onSubmit={handleNewMessage} /> 
       </div>
-      <button onClick={handleLoading} style={{ marginLeft: '220px' }}> Click me to load a poem</button>
-      <p style={{ marginLeft: '220px' }}>{data}</p>
     </div>
   );
 }
